4494acc715c49a4db2636be47480a79187cb66e4
Kris Henriksson <kthenriksson@gmail.com>
2013-11-13 15:25:10 -0500

    Update petsc to version 3.4.3
    There also needs to be a dependency on X11 for build to succeed, due to the
    superenv. It also should have a build dependency on cmake, in order to use
    the better petsc build system. (Petsc actually has 3.)

--- petsc.rb
+++ petsc.rb
@@ -2,11 +2,13 @@ require 'formula'
 
 class Petsc < Formula
   homepage 'http://www.mcs.anl.gov/petsc/index.html'
-  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.3-p5.tar.gz'
-  sha1 'ac8e64b1b266c3b8779a6dd91f21e09cf3442321'
+  url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.3.tar.gz'
+  sha1 '7458f01c003dc7381d694445b5a2ecdbca91aa57'
 
   depends_on :mpi => :cc
   depends_on :fortran
+  depends_on :x11
+  depends_on 'cmake' => :build
 
   def install
     ENV.deparallelize
