c82502f6c3dd1b5fb6706e6e375104cae3e4440c
Kris Henriksson <kthenriksson@gmail.com>
2013-11-13 15:26:42 -0500

    Add option to compile petsc with debugging enabled

--- petsc.rb
+++ petsc.rb
@@ -5,6 +5,8 @@ class Petsc < Formula
   url 'http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.4.3.tar.gz'
   sha1 '7458f01c003dc7381d694445b5a2ecdbca91aa57'
 
+  option 'with-debug', "Compile with debugging enabled"
+
   depends_on :mpi => :cc
   depends_on :fortran
   depends_on :x11
@@ -12,7 +14,10 @@ class Petsc < Formula
 
   def install
     ENV.deparallelize
-    system "./configure", "--with-debugging=0", "--prefix=#{prefix}"
+    args = ["--prefix=#{prefix}"]
+    args << "--with-debugging=0" unless build.with? 'debug'
+
+    system "./configure", *args
     system "make all"
     system "make test"
     system "make install"
